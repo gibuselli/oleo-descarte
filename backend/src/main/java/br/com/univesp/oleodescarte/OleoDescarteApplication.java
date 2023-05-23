81d8f544219a016bf3fa24cf1884f16ec1d444af
giovani <gibuselli@gmail.com>
2023-05-22 22:39:33 -0300

    add: projeto inicial spring

---
+++ backend/src/main/java/br/com/univesp/oleodescarte/OleoDescarteApplication.java
@@ -0,0 +1,13 @@
+package br.com.univesp.oleodescarte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OleoDescarteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OleoDescarteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/univesp/oleodescarte/OleoDescarteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.univesp.oleodescarte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OleoDescarteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
