ed6e168896c08514cf7ef3279d60d310966ca1eb
giovani <gibuselli@gmail.com>
2023-05-29 00:08:45 -0300

    add: [front] component de cadastro

--- frontend/oleo-descarte/src/app/app-routing.module.ts
+++ frontend/oleo-descarte/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'cadastro',
+    component: CadastroComponent
+  },
+/*   {
+    path: 'listarDoadores',
+    component: ListarDoadoresComponent
+  },
+  {
+    path: 'perfil',
+    component: PerfilComponent
+  } */
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/oleo-descarte/src/app/app.module.ts
+++ frontend/oleo-descarte/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    CadastroComponent,
   ],
   imports: [
     BrowserModule,
