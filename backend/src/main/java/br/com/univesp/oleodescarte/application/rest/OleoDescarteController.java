fe68b5d9d1861947b05672cc501532a729c81ed2
giovani <gibuselli@gmail.com>
2023-05-28 21:00:02 -0300

    add: cross origin

--- backend/src/main/java/br/com/univesp/oleodescarte/application/rest/OleoDescarteController.java
+++ backend/src/main/java/br/com/univesp/oleodescarte/application/rest/OleoDescarteController.java
@@ -1,22 +1,22 @@
 package br.com.univesp.oleodescarte.application.rest;
 
-import br.com.univesp.oleodescarte.domain.doador.Doador;
 import br.com.univesp.oleodescarte.domain.doador.DoadorService;
+
 import jakarta.validation.Valid;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.NonNull;
 import lombok.experimental.FieldDefaults;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
-@Validated
+@CrossOrigin(origins = "http://localhost:4200")
 @AllArgsConstructor(onConstructor_ = @Autowired)
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class OleoDescarteController {
